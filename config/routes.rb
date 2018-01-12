271bfb7124289f7398f2c7727794dc079bf0b74a
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-12 14:24:11 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'users/create'
 
+  get '/' => 'welcome#welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
