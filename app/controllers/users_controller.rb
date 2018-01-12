f9faaa825775be2a43fa718413a41d3746824463
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-12 14:23:05 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     @user.save
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
